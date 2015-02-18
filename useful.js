ebfcf5ad891c6b191e58536a5016b9e460d87bb7
Abhishek <abhishekpratapa@gmail.com>
2015-02-18 02:16:34 -0600

    Update useful.js

--- useful.js
+++ useful.js
@@ -3,42 +3,42 @@
 
 $(document).ready(function(){
         function centerScreen(param){
-				  	param.css("position", "absolute");
-				  	param.css("top", (($(window).height()-param.height())/2));
-				  	param.css("left", (($(window).width()-param.width())/2));
-				}
+		param.css("position", "absolute");
+		param.css("top", (($(window).height()-param.height())/2));
+		param.css("left", (($(window).width()-param.width())/2));
+	}
 				
-				function optimumLogoSize(param, scaleFactor){
-					var width = $(window).height();
-					var height = $(window).width();
-					var paramwid = param.width();
-					var paramhei = param.height();
-					var scale = 0;
-					if(width > height)
-					{
-						scale = (height*scaleFactor);
-						if(paramwid > paramhei)
-						{
-							param.css("width", "auto");
-							param.css("height", parseInt(scale*(paramhei/paramwid)));
-						}else{
-							param.css("width", scale);
-							param.css("height", "auto");
-						}
-					}else
-					{
-						scale = (width*scaleFactor);
-						if(paramwid > paramhei)
-						{
-							param.css("width", scale);
-							param.css("height", "auto");
-						}else{
-							param.css("width", "auto");
-							param.css("height", parseInt(scale*(paramwid/paramhei)));
-						}
-					}
+	function optimumLogoSize(param, scaleFactor){
+		var width = $(window).height();
+		var height = $(window).width();
+		var paramwid = param.width();
+		var paramhei = param.height();
+		var scale = 0;
+		if(width > height)
+		{
+			scale = (height*scaleFactor);
+			if(paramwid > paramhei)
+			{
+				param.css("width", "auto");
+				param.css("height", parseInt(scale*(paramhei/paramwid)));
+			}else{
+				param.css("width", scale);
+				param.css("height", "auto");
+			}
+		}else
+		{
+			scale = (width*scaleFactor);
+			if(paramwid > paramhei)
+			{
+				param.css("width", scale);
+				param.css("height", "auto");
+			}else{
+				param.css("width", "auto");
+				param.css("height", parseInt(scale*(paramwid/paramhei)));
+			}
+		}
 					
-				}
+	}
   
         function topBar(param){
         	param.css("position", "fixed");
